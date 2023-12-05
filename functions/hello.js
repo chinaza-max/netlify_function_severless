778e7c0220779a989b32bd6dcda7094e93c0db96
chinaza-max <mosesogbonna68@gmail.com>
2023-12-05 12:47:04 +0100

    pup

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,29 @@
-exports.handler = function (event, context, callback) {
-  callback(null, {
+const puppeteer = require('puppeteer');
+
+exports.handler = async function (event, context) {
+  let browser = null;
+
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    
+    // Perform some Puppeteer actions
+
+    return {
       statusCode: 200,
-      body: "Hello, world!"
-  })
-}
+      body: JSON.stringify({ message: 'Puppeteer function executed successfully' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
+
+
